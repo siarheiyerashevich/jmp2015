658952f757029879a888ab8900cfa2f42796c520
TheFallen <thefallen058@gmail.com>
2015-12-13 17:36:02 +0300

    import task1

---
+++ aka/task1/centralComponent/src/mentee/src/service/StringUtils.java
@@ -0,0 +1,12 @@
+package mentee.src.service;
+
+public class StringUtils {
+
+    public static String toUpperCase(String text) {
+        if(text == null) {
+            return null;
+        }
+        return text.toUpperCase();
+    }
+
+}
