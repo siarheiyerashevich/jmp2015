724308788592098fa90b30cd879b0f7874c611af
Maryia_Zharykava <Maryia_Zharykava@epam.com>
2016-02-08 10:46:34 +0300

    add task to module9

---
+++ mzh/task2/Module9_jre6/Module9_jre6/src/com/epam/module9/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.epam.module9.controller;
+
+import com.epam.module9.collect.CollectList;
+
+public class Controller {
+	public static void main(String[] args) {
+		CollectList.readFile();
+	}
+}
