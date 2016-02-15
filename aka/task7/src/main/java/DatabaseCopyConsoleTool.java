7c0cd62ba556f8161c374364892c75e9a8a7dcad
TheFallen <thefallen058@gmail.com>
2016-02-15 22:11:33 +0300

    -task7 subtask done

---
+++ aka/task7/src/main/java/DatabaseCopyConsoleTool.java
@@ -0,0 +1,15 @@
+package task7.src.main.java;
+
+/**
+ * Created by TheFallen on 2/10/16.
+ */
+public class DatabaseCopyConsoleTool {
+
+    private static final String HOST = "jdbc:mysql://localhost:3306/mentee?autoReconnect=true&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "pass";
+
+    public static void main(String[] args) {
+
+    }
+}
