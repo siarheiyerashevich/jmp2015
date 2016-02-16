ccb93ea240383088a5a00fdba11433ed18a0b8d5
TheFallen <thefallen058@gmail.com>
2016-02-16 10:46:29 +0300

    -task7 added table and column creation

--- aka/task7/src/main/java/DatabaseCopyConsoleTool.java
+++ aka/task7/src/main/java/DatabaseCopyConsoleTool.java
@@ -7,13 +7,17 @@ import java.sql.*;
  */
 public class DatabaseCopyConsoleTool {
 
-    private static final String HOST = "jdbc:mysql://localhost:3306/mentee?autoReconnect=true&useSSL=false";
+    private static final String SOURCE_HOST = "jdbc:mysql://localhost:3306/mentee?autoReconnect=true&useSSL=false";
+    private static final String TARGET_HOST = "jdbc:mysql://localhost:3306/target?autoReconnect=true&useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "pass";
-    private static final String SCHEMA = "mentee";
+    private static final String SOURCE_SCHEMA = "mentee";
+    private static final String TARGET_SCHEMA = "target";
 
-    private static Statement statement = null;
-    private static Connection connection = null;
+    private static Statement sourceStatement = null;
+    private static Statement targetStatement = null;
+    private static Connection sourceConnection = null;
+    private static Connection targetConnection = null;
 
     public static void main(String[] args) {
 
@@ -21,14 +25,40 @@ public class DatabaseCopyConsoleTool {
             Class.forName("com.mysql.jdbc.Driver").getInterfaces();
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 
-            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
-            System.out.println(connection.toString());
-            statement = connection.createStatement();
+            sourceConnection = DriverManager.getConnection(SOURCE_HOST, USERNAME, PASSWORD);
+            sourceStatement = sourceConnection.createStatement();
+            targetConnection = DriverManager.getConnection(TARGET_HOST, USERNAME, PASSWORD);
+            targetStatement = targetConnection.createStatement();
 
-            DatabaseMetaData md = connection.getMetaData();
-            ResultSet rs = md.getTables(null, null, "%", null);
-            while (rs.next()) {
-                System.out.println(rs.getString(3));
+            DatabaseMetaData md = sourceConnection.getMetaData();
+            ResultSet tablesRs = md.getTables(null, null, "%", null);
+            while (tablesRs.next()) {
+                final String tableName = tablesRs.getString(3);
+                // get column names from curent table
+                StringBuilder columnsSb = new StringBuilder();
+                columnsSb.append("SHOW COLUMNS FROM ");
+                columnsSb.append(tableName);
+                columnsSb.append(" FROM ");
+                columnsSb.append(SOURCE_SCHEMA);
+                ResultSet columnRs = sourceStatement.executeQuery(columnsSb.toString());
+
+                StringBuilder newTableSb = new StringBuilder();
+                newTableSb.append("CREATE TABLE ");
+                newTableSb.append(tableName);
+                newTableSb.append(" ( ");
+                while (columnRs.next()) {
+                    final String columName = columnRs.getString("Field");
+                        newTableSb.append(columName);
+                        newTableSb.append(" varchar(255)");
+                        if (!columnRs.isLast()) {
+                            newTableSb.append(", ");
+                        }
+                }
+                newTableSb.append(" ); ");
+                targetStatement.execute(newTableSb.toString());
+
+
+
             }
 
         } catch (ClassNotFoundException | SQLException e) {
