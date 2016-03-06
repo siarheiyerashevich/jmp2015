c90fd67ce1f55d8484c9b121dc01f8c122b70f27
TheFallen <thefallen058@gmail.com>
2016-03-06 19:13:28 +0300

    -task8 done

---
+++ aka/task8/task8.impl/src/main/java/SomeServiceImpl.java
@@ -0,0 +1,10 @@
+/**
+ * Created by TheFallen on 2/25/16.
+ */
+public class SomeServiceImpl implements SomeService {
+
+    @Override
+    public String getSomeText() {
+        return "SomeText";
+    }
+}
